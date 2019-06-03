437e20cc0a343d72e78d23ce9192950c5eb648d3
CiberGhost <malcom.hugler@outlook.com>
2019-06-03 16:47:18 +0200

    Add Windows support to premake5.lua.

--- premake5.lua
+++ premake5.lua
@@ -42,6 +42,28 @@ project "GLFW"
         defines
         {
             "_GLFW_X11"
+		}
+		
+	filter "system:windows"
+        files
+        {
+            "src/win32_joystick.c",
+
+            "src/win32_init.c",
+            "src/win32_monitor.c",
+            "src/win32_window.c",
+
+            "src/win32_time.c",
+            "src/win32_thread.c",
+
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines
+        {
+            "_GLFW_WIN32"
         }
 
     filter "configurations:Debug"
